b567e7d131e37e27da4821b4bc6270daa8442296
gwelle <guillaume.welle.sio@gmail.com>
2025-03-17 14:44:52 +0100

    first routes (POST)

--- index.js
+++ index.js
@@ -2,10 +2,31 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const data = require('./posts.json') ; // Importer le fichier data.json
+
+app.use(express.json()) ; // Middleware pour parser le body en JSON
+
+let posts = [] ; // Mémoire local pour stocker des objets post
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Welcome')
+  return "Welcome" ;
+})
+
+app.get('/posts', (req, res) => {
+    
+    // Concaténer les deux tableaux
+    // const data_posts = posts.concat(data) ;
+    const data_posts = [...posts, ...data] ; 
+    res.json(data_posts) ;
+})
+
+app.post('/posts/create', (req, res) => {
+
+  posts = [...posts, req.body];
+  res.send("Post added successfully ") ;
+  
 })
 
 app.listen(port, () => {
