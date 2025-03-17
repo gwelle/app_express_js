32f6dadb9b7acfb266fdde43e1fc56205c147d64
gwelle <guillaume.welle.sio@gmail.com>
2025-03-17 13:17:23 +0100

    express js app basics

---
+++ index.js
@@ -0,0 +1,13 @@
+// Description: This is the main file for the project.
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
